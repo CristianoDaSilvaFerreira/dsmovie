949d8cb227534c77efa888687dff614cf9d8f18e
Cristiano Ferreira <cristianodevsystem@gmail.com>
2022-01-16 23:50:54 -0300

    :rocket: Correções finais  - Front-End Finalizado

--- frontend/src/pages/Form/Form.tsx
+++ frontend/src/pages/Form/Form.tsx
@@ -2,12 +2,9 @@ import FormCard from "components/FormCard";
 import { useParams } from "react-router-dom";
 
 function Form() {
-
   const params = useParams();
-  
-  return (
-    <FormCard movieId={`${params.movieId}`} />
-  );
+
+  return <FormCard movieId={`${params.movieId}`} />;
 }
 
 export default Form;
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,10 +1,9 @@
 import axios from "axios";
+import { useState, useEffect } from "react";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
-import { useEffect, useState } from "react";
-import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
-import "./styles.css";
+import { MoviePage } from "types/movie";
 
 function Listing() {
   const [pageNumber, setPageNumber] = useState(0);
@@ -23,16 +22,16 @@ function Listing() {
 
   useEffect(() => {
     axios
-      .get(`${BASE_URL}/movies?size=12?page=${pageNumber}`)
+      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
       .then((response) => {
         const data = response.data as MoviePage;
         setPage(data);
       });
   }, [pageNumber]);
 
-  const handlePageChange = (newPageNumber : number) => {
+  const handlePageChange = (newPageNumber: number) => {
     setPageNumber(newPageNumber);
-  }
+  };
 
   return (
     <>
@@ -40,13 +39,11 @@ function Listing() {
 
       <div className="container">
         <div className="row">
-
           {page.content.map((movie) => (
             <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
               <MovieCard movie={movie} />
             </div>
           ))}
-
         </div>
       </div>
     </>
