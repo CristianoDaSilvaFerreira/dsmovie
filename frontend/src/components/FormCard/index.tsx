48f05d584fae2388546d892fb08ebcba2c7e5748
Cristiano Ferreira <cristianodevsystem@gmail.com>
2022-01-16 22:58:51 -0300

    :up: rename FormCard

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -1,6 +1,6 @@
-import axios, { AxiosRequestConfig } from 'axios';
+import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Movie } from 'types/movie';
 import { BASE_URL } from 'utils/requests';
 import './styles.css';
@@ -11,7 +11,7 @@ type Props = {
 
 function FormCard( { movieId } : Props) {
 
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
     const [movie, setMovie] = useState<Movie>();
 
@@ -22,39 +22,39 @@ function FormCard( { movieId } : Props) {
             });
     }, [movieId]);
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    // const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 
-        event.preventDefault();
+    //     event.preventDefault();
 
-        const email = (event.target as any).email.value;
-        const score = (event.target as any).score.value;
+    //     const email = (event.target as any).email.value;
+    //     const score = (event.target as any).score.value;
 
-        // if (!validateEmail(email)) {
-        //     return;
-        // }
+    //     if (!validateEmail(email)) {
+    //         return;
+    //     }
 
-        const config: AxiosRequestConfig = {
-            baseURL: BASE_URL,
-            method: 'PUT',
-            url: '/scores',
-            data: {
-                email: email,
-                movieId: movieId,
-                score: score
-            }
-        }
+    //     const config: AxiosRequestConfig = {
+    //         baseURL: BASE_URL,
+    //         method: 'PUT',
+    //         url: '/scores',
+    //         data: {
+    //             email: email,
+    //             movieId: movieId,
+    //             score: score
+    //         }
+    //     }
 
-        axios(config).then(response => {
-            navigate("/");
-        });
-    }
+    //     axios(config).then(response => {
+    //         navigate("/");
+    //     });
+    // }
 
     return (
         <div className="dsmovie-form-container">
             <img className="dsmovie-movie-card-image" src={movie?.image} alt={movie?.title} />
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie?.title}</h3>
-                <form className="dsmovie-form" onSubmit={handleSubmit}>
+                <form className="dsmovie-form" >
                     <div className="form-group dsmovie-form-group">
                         <label htmlFor="email">Informe seu email</label>
                         <input type="email" className="form-control" id="email" />
